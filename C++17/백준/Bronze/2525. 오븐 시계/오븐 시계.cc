44fef477d009156525257436361b3870013fde10
minjae2002 <121847472+minjae2002@users.noreply.github.com>
2024-07-23 17:40:42 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/2525. 오븐 시계/오븐 시계.cc
+++ C++17/백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -1,26 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int A, B, C;
-	cin >> A >> B;
-	cin >> C;
-	
-	int C_H = C / 60;
-	int C_M = C % 60;
-	
-	if (B + C_M >= 60){
-		B = B + C_M - 60;
-		A = (A + C_H + 1) % 24;
-	}
-	else {
-		B = B + C_M;
-		if (A + C_H >= 24)
-			A = A + C_H - 24;
-		else
-			A = A + C_H;
-	}
-	cout << A << " " << B << endl;
-	
-	return 0;
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    int total_minutes = A * 60 + B;
+    
+    total_minutes += C;
+    
+    int H = (total_minutes / 60) % 24;
+    int M = total_minutes % 60;
+
+    cout << H << " " << M << endl;
+
+    return 0;
 }
