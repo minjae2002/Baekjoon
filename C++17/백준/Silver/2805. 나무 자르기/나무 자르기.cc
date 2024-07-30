65836c6c6a72168df2ecb8372228cc5d46f4a2c5
minjae2002 <121847472+minjae2002@users.noreply.github.com>
2024-07-30 20:59:31 +0900

    [Silver II] Title: 나무 자르기, Time: 224 ms, Memory: 5928 KB -BaekjoonHub

---
+++ C++17/백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAXN 1000000
+#define MAXSIZE 2000000000
+
+int N, M; //나무의 수 N, 집으로 가져가려고 하는 나무의 길이 M
+int Wood[MAXN]; //입력되는 나무의 길이들 저장 
+
+bool fmax(int size){
+	long long h = 0;
+	//나무를 size만큼 자르고 남은 나머지를 더한다.
+	for (int i = 0; i < N;i++) h += max(0, Wood[i] - size);
+	if (h >= M) return true;
+	else return false; 
+} 
+
+int main(void){
+	int lb = 0, ub = 0, m, ans;
+	
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++){
+		scanf("%d", &Wood[i]);
+		ub = max(ub, Wood[i]);
+	}
+	
+	while(lb + 1 < ub){
+		m = lb + (ub - lb) / 2;
+		if (fmax(m)) lb = m;
+		else ub = m;
+	}
+	printf("%d", lb);
+	return 0;
+}
