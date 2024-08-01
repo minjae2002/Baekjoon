a10985d65b8f1e803b10e3ec9a24f4c1c3d5fad1
minjae2002 <121847472+minjae2002@users.noreply.github.com>
2024-08-02 00:36:46 +0900

    Update 가운데를 말해요.cc

--- C++17/백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
+++ C++17/백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
@@ -1,24 +1,25 @@
-#include <bits/stdc++.h>
+// 2 heaps Algorithm ? 
 
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    priority_queue<int> maxheap, minheap;
-
+int main(){
+    priority_queue<int> maxheap;			// 입력된 숫자 중 작은 절반 저장하는 최대 힙 
+	priority_queue<int> minheap;			// 입력된 숫자 중 큰 절반 저장하는 최소 힙 
+	
+	// priority_queue 자료형이므로 힙에 원소들이 들어오면 
+	// 자동적으로 정렬되어 top 원소가 정리된다! 
+	
     int N;
     scanf("%d", &N);
 
-    for(int i=0, num; i<N; i++)
-    {
+    for(int i = 0, num; i < N; i++){
         scanf("%d", &num);
         if(maxheap.size() > minheap.size()) minheap.push(-num);
         else maxheap.push(num);
         
-        if(maxheap.size()>0 && minheap.size()>0)
-        {
-            if(maxheap.top() > -minheap.top())
-            {
+        if(maxheap.size() > 0 && minheap.size() > 0){
+            if(maxheap.top() > -minheap.top()){
                 int maxtop = maxheap.top();
                 int mintop = -minheap.top();
                 maxheap.pop();
@@ -29,6 +30,5 @@ int main()
         }
         printf("%d\n", maxheap.top());
     }
-
     return 0;
 }
