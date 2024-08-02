06111a8c9b5568f86e18df05ec46e09a53e42b20
minjae2002 <121847472+minjae2002@users.noreply.github.com>
2024-08-02 21:59:32 +0900

    Update 에라토스테네스의 체.cc

--- C++17/백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
+++ C++17/백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
@@ -3,15 +3,15 @@ using namespace std;
 
 #define MAXN 1001
 
-bool Visit[MAXN];
-bool check(int &cnt, int &K, int &i){
-	if (++cnt == K){
-		printf("%d", i);
-		return true;
+bool Visit[MAXN];								// 소수 여부를 기록하는 배열. 초기는 모두 false
+bool check(int &cnt, int &K, int &i){						// cnt: 현재까지 체에서 삭제된 수, K: 찾아야하는 순번, i: 현재 확인 중인 수 
+	if (++cnt == K){							// cnt를 증가시키며, K와 같은지 비교 
+		printf("%d", i);						// K와 같다면 현재 숫자 i를 출력 
+		return true;							// K번째 삭제된 숫자를 찾았음을 표시. 
 	}
-	Visit[i] = true;
+	Visit[i] = true;							// cnt != K이더라도 현재 숫자 i는 배열에서 삭제. 
 	
-	return false;
+	return false;								// K번째 삭제된 수를 못 찾았다면 false. 
 }
 
 int main(){
@@ -19,11 +19,11 @@ int main(){
 	scanf("%d %d", &N, &K);
 	
 	for (int i = 2; i <= N; i++){
-		if (Visit[i]) continue;
-		else if (check(cnt, K, i)) return 0;
+		if (Visit[i]) continue;						// Visit[i] = true라면 이미 체에서 삭제한 수라는 뜻. continue를 통해 루프의 다음 반복으로 넘어감. 
+		else if (check(cnt, K, i)) return 0;				// Visit[i] = false라면 아직 삭제되지 않은 수. 이게 K번째 삭제될 수인지 check한다. 
 		
-		for (int j = i * i; j <= N; j += i){
-			if (!Visit[j] && check(cnt, K, j)) return 0;
+		for (int j = i * i; j <= N; j += i){				// i의 배수들에 대하여 
+			if (!Visit[j] && check(cnt, K, j)) return 0;		// !Visit[i] (아직 삭제되지 않음) && check(cnt, K, j) = 1 (K번째 숫자 찾음) -> 프로그램 종료 
 		}
 	}
 	return 0;
