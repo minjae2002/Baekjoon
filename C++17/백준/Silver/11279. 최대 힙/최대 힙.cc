972f3ca3ea098e0e3893f81f37a1b53bae4ab105
minjae2002 <121847472+minjae2002@users.noreply.github.com>
2024-08-05 12:37:16 +0900

    [Silver II] Title: 최대 힙, Time: 20 ms, Memory: 2800 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+priority_queue<int> A; 
+
+int main(void){
+	int N;
+	scanf("%d", &N);
+	
+	for (int i = 0; i < N; i++){
+		ll x;
+		scanf("%lld", &x);
+		
+		if (x != 0){
+			A.push(x);
+		} 
+		else {
+			if (A.empty()) printf("0\n");
+			else {
+				ll a = A.top();
+				printf("%lld\n", a);
+				A.pop();
+			}
+		}
+	}
+	return 0;
+}
