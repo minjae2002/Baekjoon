6944fbc91434727272478f2d0859daccbcded2b4
minjae2002 <121847472+minjae2002@users.noreply.github.com>
2025-10-06 01:22:02 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    while (true){
+        scanf("%d %d %d", &a, &b, &c);
+        if (a == 0 && b == 0 && c == 0) break;
+
+        if (a == b && b == c) printf("Equilateral\n");
+        else if (a + b <= c || a + c <= b || b + c <= a) printf("Invalid\n");
+        else if (a == b || b == c || a == c) printf("Isosceles\n");
+        else printf("Scalene\n");
+    }
+}
